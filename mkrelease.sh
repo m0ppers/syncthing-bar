b20ebce57089e477fb8c89f5e67857addaec90f3
Andreas Streichardt <andreas.streichardt@gmail.com>
2014-12-20 11:37:20 +0100

    Add version stuff

--- mkrelease.sh
+++ mkrelease.sh
@@ -2,4 +2,4 @@
 
 xcodebuild install
 pkgbuild --analyze --root /tmp/syncthing-bar.dst syncthing-bar.plist
-pkgbuild --root /tmp/syncthing-bar.dst --component-plist syncthing-bar.plist --scripts scripts syncthing-bar.pkg
+pkgbuild --root /tmp/syncthing-bar.dst --component-plist syncthing-bar.plist --scripts scripts --version $1 syncthing-bar-$1.pkg
