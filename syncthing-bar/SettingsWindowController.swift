1609182dd6ef105e699b81b973c5f726e1a77186
Chris Russ <chruss@gmx.de>
2015-06-03 15:45:30 +0200

    making app monitoring optional and disable by default

--- syncthing-bar/SettingsWindowController.swift
+++ syncthing-bar/SettingsWindowController.swift
@@ -13,6 +13,7 @@ class SettingsWindowController: NSWindowController {
     @IBOutlet var invert_icon_check: NSButton!
     @IBOutlet var port_field: NSTextField!
     @IBOutlet var confirm_exit_check: NSButton!
+    @IBOutlet var monitoring_check: NSButton!
     @IBOutlet var monitor_apps: NSTextField!
     
     var settings: SyncthingSettings = SyncthingSettings()
@@ -77,6 +78,12 @@ class SettingsWindowController: NSWindowController {
     
     @IBAction func confirm_exit_checked(sender: NSButton) {
         self.settings.confirm_exit = (sender.state == NSOnState)
+        
+        postSettings()
+    }
+    
+    @IBAction func monitoring_checked(sender: NSButton) {
+        self.settings.monitoring = (sender.state == NSOnState)
         
         postSettings()
     }
--- syncthing-bar/SyncthingSettings.swift
+++ syncthing-bar/SyncthingSettings.swift
@@ -23,6 +23,9 @@ class SyncthingSettings {
     var confirm_exit_key: String = "ConfirmExit"
     var confirm_exit: Bool = true
     
+    var monitoring_key: String = "Monitoring"
+    var monitoring: Bool = false
+    
     var monitor_apps_key: String = "MonitorApps"
     var monitor_apps = "iPhoto; iMovie\nLightroom"
     
@@ -31,22 +34,25 @@ class SyncthingSettings {
         self.invert_icon_key = "InvertIcon"
         self.port_key = "PortNumber"
         self.confirm_exit_key = "ConfirmExit"
+        self.monitoring_key = "Monitoring"
         self.monitor_apps_key = "MonitorApps"
         
         self.bw_icon = true
         self.invert_icon = false
         self.port = "8084"
         self.confirm_exit = true
+        self.monitoring = false
         self.monitor_apps = "iPhoto; iMovie\nLightroom"
         
         self.loadSettings()
     }
     
-    init(bw_icon: Bool, invert_icon: Bool, port: String, confirm_exit: Bool, monitor_apps: String) {
+    init(bw_icon: Bool, invert_icon: Bool, port: String, confirm_exit: Bool, monitoring: Bool, monitor_apps: String) {
         self.bw_icon = bw_icon
         self.invert_icon = invert_icon
         self.port = port
         self.confirm_exit = confirm_exit
+        self.monitoring = monitoring
         self.monitor_apps = monitor_apps
         
         self.saveSettings()
@@ -71,6 +77,10 @@ class SyncthingSettings {
             self.confirm_exit = confirm_exit
         }
         
+        if let monitoring = defaults.valueForKey(self.monitoring_key) as? Bool {
+            self.monitoring = monitoring
+        }
+        
         if let monitor_apps_string = defaults.valueForKey(self.monitor_apps_key) as? String {
             self.monitor_apps = monitor_apps_string
         }
@@ -85,6 +95,7 @@ class SyncthingSettings {
         defaults.setValue(self.invert_icon, forKey: self.invert_icon_key)
         defaults.setValue(self.port, forKey: self.port_key)
         defaults.setValue(self.confirm_exit, forKey: self.confirm_exit_key)
+        defaults.setValue(self.monitoring, forKey: self.monitoring_key)
         defaults.setValue(self.monitor_apps, forKey: self.monitor_apps_key)
         
         defaults.synchronize()
@@ -109,6 +120,12 @@ class SyncthingSettings {
             wndCtrl.confirm_exit_check?.state = NSOnState
         } else {
             wndCtrl.confirm_exit_check?.state = NSOffState
+        }
+        
+        if (self.monitoring) {
+            wndCtrl.monitoring_check?.state = NSOnState
+        } else {
+            wndCtrl.monitoring_check?.state = NSOffState
         }
         
         wndCtrl.monitor_apps?.stringValue = self.monitor_apps as String
